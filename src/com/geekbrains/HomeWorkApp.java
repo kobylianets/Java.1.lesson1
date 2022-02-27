dbbea76fb4fa4e750f46447f6ac14747c0064951
Olga Kobylianets <kobylianets@gmail.com>
2022-02-27 19:29:35 +0300

    Initial commit

---
+++ src/com/geekbrains/HomeWorkApp.java
@@ -0,0 +1,61 @@
+package com.geekbrains;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        cheсkSumSign();
+        printColor ();
+        compareNumbers();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    private static void cheсkSumSign() {
+        int a = 5;
+        int b = - 10;
+        int c = a + b;
+
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    private static void printColor() {
+        int value = 200;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        else if (value > 0 && value <=100 ) {
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
+
+    }
+    private static void compareNumbers () {
+        int a = 17;
+        int b = 15;
+
+        if (a >= b) {
+            System.out.println("a>=b");
+        }
+        else {
+            System.out.println("a<b");
+        }
+
+    }
+
+
+}
+
+
